4ab6bd5a1d02936c4f63fc4689b76cbbffb893f6
Oleksandr Kaiumov <66486758+Redpaw-Game-Dev@users.noreply.github.com>
2023-02-05 09:29:12 +0200

    [TZZ-5] Ball auto input
    Ball movement bug after exit to menu is fixed

---
+++ Assets/Scripts/StateMachineSystem/Specific/Conditions/GameModePlayerCondition.cs
@@ -0,0 +1,17 @@
+﻿using TestZigZag.Game;
+using Zenject;
+
+namespace TestZigZag.StateMachineSystem
+{
+    public class GameModePlayerCondition : Condition
+    {
+        [Inject] private GameManager _gameManager;
+        
+        public override bool IsFulfilled()
+        {
+            return _logicalNot 
+                ? _gameManager.GameMode != GameMode.Player 
+                : _gameManager.GameMode == GameMode.Player;
+        }
+    }
+}
